2d2b788a2d0244f195d7a500a5e422382bb781c3
BYUIOMDA <and22099@byui.edu>
2023-09-24 21:27:52 -0600

    0.1.37
    color change

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,4 +1,18 @@
-function toggleDarkMode() {
-    const main = document.querySelector('main');
-    main.classList.toggle('dark-mode');
-  }
+const modeButton = document.querySelector('#mode');
+const body = document.querySelector('body');
+const main = document.querySelector('main');
+const nav = document.querySelector('nav');
+
+modeButton.addEventListener('click', () => {
+    if (modeButton.textContent.includes('Darkmode Toggle ☑️')) {
+        body.style.backgroundColor = '#0D1B2A'; // Dark Blue
+        main.style.color = '#FFFFFF'; // White
+        modeButton.textContent = 'Darkmode Toggle ❎';
+        nav.style.backgroundColor = '#002E5D'; // BYU Blue
+    } else {
+        body.style.backgroundColor = '#FFFFFF'; // White
+        main.style.color = '#0D1B2A'; // Dark Blue
+        modeButton.textContent = 'Darkmode Toggle ☑️';
+        nav.style.backgroundColor = '#002E5D'; // BYU Blue
+    }
+});
