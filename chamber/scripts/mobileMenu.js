cfb119c42ad5e12238be2fab7bd9eca66007caea
BYUIOMDA <business.matthewdanderson@gmail.com>
2023-10-01 23:31:02 -0600

    0.3.7
    please for the love, work

--- chamber/scripts/mobileMenu.js
+++ chamber/scripts/mobileMenu.js
@@ -12,4 +12,24 @@ function toggleMenu() {
       closeButton.style.display = "block";
       hamburgerButton.style.display = "none";
     }
-  }
+    var menuButton = document.getElementById("menu");
+    var menuItems = document.getElementById("menu-items");
+    
+    menuButton.addEventListener("click", function() {
+        if (menuItems.style.display === "block") {
+        menuItems.style.display = "none";
+        } else {
+        menuItems.style.display = "block";
+        }
+    })
+    var menuButton = document.getElementById("menu");
+    var menuItems = document.querySelector(".navigation");
+
+    menuButton.addEventListener("click", function() {
+    if (menuItems.classList.contains("show")) {
+        menuItems.classList.remove("show");
+    } else {
+        menuItems.classList.add("show");
+    }
+    })
+};
