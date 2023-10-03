5facb9b994b600706e93af5b04b0bb9ea408ed10
BYUIOMDA <business.matthewdanderson@gmail.com>
2023-10-02 21:20:46 -0600

    0.3.8
    chatgptsave me

--- chamber/scripts/mobileMenu.js
+++ chamber/scripts/mobileMenu.js
@@ -1,35 +1,7 @@
-function toggleMenu() {
-    var menuItems = document.getElementById("menu-items");
-    var closeButton = document.getElementById("close-button");
-    var hamburgerButton = document.getElementById("hamburger-button");
-    
-    if (menuItems.style.display === "block") {
-      menuItems.style.display = "none";
-      closeButton.style.display = "none";
-      hamburgerButton.style.display = "block";
-    } else {
-      menuItems.style.display = "block";
-      closeButton.style.display = "block";
-      hamburgerButton.style.display = "none";
-    }
-    var menuButton = document.getElementById("menu");
-    var menuItems = document.getElementById("menu-items");
-    
-    menuButton.addEventListener("click", function() {
-        if (menuItems.style.display === "block") {
-        menuItems.style.display = "none";
-        } else {
-        menuItems.style.display = "block";
-        }
-    })
-    var menuButton = document.getElementById("menu");
-    var menuItems = document.querySelector(".navigation");
+const mainNav = document.querySelector('.navigation');
+const hamBtn = document.querySelector('#menu');
 
-    menuButton.addEventListener("click", function() {
-    if (menuItems.classList.contains("show")) {
-        menuItems.classList.remove("show");
-    } else {
-        menuItems.classList.add("show");
-    }
-    })
-};
+hamBtn.addEventListener('click', () => {
+    mainNav.classList.toggle('show');
+    hamBtn.classList.toggle('show');
+})
